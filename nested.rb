50ed837c4133ce57a191c00c142bd9f0711a8cbb
Sara Nolen <nolensara1@gmail.com>
2021-01-10 16:00:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,7 +60,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages]
+programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
